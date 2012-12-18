edef77bd7777e1ea3b6bad9eeaf92106dc17bf26
Gregory <galymn@gmail.com>
2012-12-18 21:16:40 +0100

    Test suite

--- json-dsl/src/test/java/json/builder/BuildersTest.java
+++ json-dsl/src/test/java/json/builder/BuildersTest.java
@@ -10,6 +10,9 @@ import json.lang.JsonValue.Object;
 
 import static json.lang.JsonValue.ValueKind.ARRAY;
 import static json.lang.JsonValue.ValueKind.OBJECT;
+import static json.lang.JsonValue.ValueKind.NUMBER;
+import static json.lang.JsonValue.ValueKind.STRING;
+import static json.lang.JsonValue.ValueKind.TRUE;
 
 import static org.junit.Assert.*;
 
@@ -18,53 +21,59 @@ import org.junit.Test;
 
 public class BuildersTest {
 
-	@Test
-	public void ArrayBuilderTest() {
+    @Test
+    public void ArrayBuilderTest() {
 
-		// Empty Array
-		Array emptyArray = jsonArray().build();
-		assertNotNull(emptyArray);
-		assertEquals(ARRAY, emptyArray.getValueKind());
-		assertTrue(CollectionUtils.isEmpty(emptyArray.getValue()));
+        // Empty Array
+        Array emptyArray = jsonArray().build();
+        assertNotNull(emptyArray);
+        assertEquals(ARRAY, emptyArray.getValueKind());
+        assertTrue(CollectionUtils.isEmpty(emptyArray.getValue()));
 
-		// Array with values
-		BigDecimal number = new BigDecimal(1890734.589);
-		boolean vrai = true;
-		String foo = "Foo";
-		Array array = jsonArray().add(number).add(vrai).add(foo).build();
-		assertNotNull(array);
-		assertEquals(ARRAY, array.getValueKind());
-		assertTrue(array.getValue().size() == 3);
-		assertTrue(number.equals(array.getValue().get(0).getValue()));
-		assertTrue(foo.equals(array.getValue().get(2).getValue()));
-		
-		// Adding Array
-		
-		array = jsonArray().add(number).add(vrai).add(foo).startArray().add(-7894561230L).endArray().build();
-	}
+        // Array with values
+        BigDecimal number = new BigDecimal(1890734.589);
+        boolean vrai = true;
+        String foo = "Foo";
+        Array array = jsonArray().add(number).add(vrai).add(foo).build();
+        assertNotNull(array);
+        assertEquals(ARRAY, array.getValueKind());
+        assertTrue(array.getValue().size() == 3);
+        assertTrue(number.equals(array.getValue().get(0).getValue()));
+        assertTrue(TRUE.equals(array.getValue().get(1).getValueKind()));
+        assertTrue(foo.equals(array.getValue().get(2).getValue()));
+        assertTrue(STRING.equals(array.getValue().get(2).getValueKind()));
 
-	@Test
-	public void ObjectBuilderTest() {
+        // Adding Array
 
-		// Empty Object
-		Object emptyObject = jsonObject().build();
-		assertNotNull(emptyObject);
-		assertEquals(OBJECT, emptyObject.getValueKind());
-		assertTrue(CollectionUtils.isEmpty(emptyObject.getValue()));
+        array = jsonArray().add(number).add(vrai).add(foo).startArray().add(-7894561230L).endArray().build();
+    }
 
-		// Object with values
-		BigDecimal number = new BigDecimal(1890734.589);
-		boolean vrai = true;
-		String foo = "Foo";
-		Object object = jsonObject().add("number", number).add("vrai", vrai)
-				.add("foo", foo).build();
-		assertNotNull(object);
-		assertEquals(OBJECT, object.getValueKind());
-		assertTrue(object.getValue().size() == 3);
-		assertTrue(number.equals(object.getValue().get(0).getValue().getValue()));
-		assertTrue("number".equals(object.getValue().get(0).getName()));
-		assertTrue(foo.equals(object.getValue().get(2).getValue().getValue()));
-		assertTrue("foo".equals(object.getValue().get(2).getName()));
-	}
+    @Test
+    public void ObjectBuilderTest() {
+
+        // Empty Object
+        Object emptyObject = jsonObject().build();
+        assertNotNull(emptyObject);
+        assertEquals(OBJECT, emptyObject.getValueKind());
+        assertTrue(CollectionUtils.isEmpty(emptyObject.getValue()));
+
+        // Object with values
+        BigDecimal number = new BigDecimal(1890734.589);
+        boolean vrai = true;
+        String foo = "Foo";
+        Object object = jsonObject().add("number", number).add("vrai", vrai).add("foo", foo).build();
+        assertNotNull(object);
+        assertEquals(OBJECT, object.getValueKind());
+        assertTrue(object.getValue().size() == 3);
+        assertTrue(number.equals(object.getValue().get(0).getValue().getValue()));
+        assertTrue("number".equals(object.getValue().get(0).getName()));
+        assertTrue(NUMBER.equals(object.getValue().get(0).getValue().getValueKind()));
+        assertTrue((Boolean) object.getValue().get(1).getValue().getValue());
+        assertTrue("vrai".equals(object.getValue().get(1).getName()));
+        assertTrue(TRUE.equals(object.getValue().get(1).getValue().getValueKind()));
+        assertTrue(foo.equals(object.getValue().get(2).getValue().getValue()));
+        assertTrue("foo".equals(object.getValue().get(2).getName()));
+        assertTrue(STRING.equals(object.getValue().get(2).getValue().getValueKind()));
+    }
 
 }
